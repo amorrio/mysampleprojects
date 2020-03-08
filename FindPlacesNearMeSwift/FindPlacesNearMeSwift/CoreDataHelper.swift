c17949c4f8cab95e92692a28aa80c265aa47f46e
amorcrio <amorcorazon.rio@gmail.com>
2020-03-09 07:11:50 +0800

    Cleanup and add comments

--- FindPlacesNearMeSwift/FindPlacesNearMeSwift/CoreDataHelper.swift
+++ FindPlacesNearMeSwift/FindPlacesNearMeSwift/CoreDataHelper.swift
@@ -11,79 +11,79 @@ import CoreLocation
 
 class CoreDataHelper : NSObject {
     var managedObjectContext : NSManagedObjectContext
-    
+
     init(context: NSManagedObjectContext){
         self.managedObjectContext = context
     }
-    
+
     /**
     Returns an array of `Place` objects created by parsing  the `searchResult` dictionary generated from  the JSON response
     to a query of the places .demo.api REST API
     Automatically saves extracted data  to the database
-     
+
      - Parameter searchResult: The dictionary result from the places.demo.api REST API
      - Parameter searchString: The search key used to get the search result.
      - Parameter coordinate: The  coordinates of the current user location when the search is performed.
-     
-     - Returns: An array `Place` objects parsed from the `searchResult`.`nil` if search result is not successfully parsed or if no data was extracted from the result
+
+     - Returns: An array `Place` objects parsed from the `searchResult`.`nil` if search result is not successfully parsed
      */
-    
+
     func placesForSearchResult(searchResult: [String:AnyObject]?, searchString: String, coordinate: CLLocationCoordinate2D ) -> [Place]?
     {
         var places : [Place] = []
-        
+
         guard let result = searchResult?["results"] as? [String:AnyObject] else
         {
             return nil
         }
-        
+
         guard let placeItems = result["items"] as? [AnyObject] else
         {
             return nil
         }
-        
+
         let sourceSearch = self.userLocationSearch(searchString: searchString, coordinate:coordinate, createNewIfNone:true)
-        
+
         for placeItem in placeItems
         {
             guard let placeItemDict  = placeItem as? [String:AnyObject] else {
                 continue
             }
-            
+
             let title = placeItemDict["title"] as? String ?? ""
             var vicinity = placeItemDict["vicinity"] as? String
-            
+
             if vicinity != nil
             {
                 vicinity = vicinity?.replacingOccurrences(of: "<br/>", with: ", ") ?? ""
             }
-            
+
             guard let distance = placeItemDict["distance"] as? NSNumber else {
                 continue
             }
-            
+
             guard let position =  placeItemDict["position"] as? [NSNumber]
                 else {
                     continue
             }
-            
+
             if position.count < 2 {
                 continue
             }
-            
+
             guard let place = NSEntityDescription.insertNewObject(forEntityName: "Place", into: self.managedObjectContext) as? Place
                 else {
                     self.rollbackDatabase()
                     return nil
             }
-            
+
             place.title = title
             place.vicinity = vicinity
             place.latitude = position[0].doubleValue
             place.longitude = position[1].doubleValue
             place.distance = distance.doubleValue
             place.sourceSearch = sourceSearch
-            
+
             places.append(place)
         }
         if places.count > 0 {
@@ -91,35 +91,34 @@ class CoreDataHelper : NSObject {
         }
         else {
             self.rollbackDatabase()
-            return nil
         }
-        
+
         return places
     }
-    
+
     /**
      Returns an array of `Place` objects that were saved for a specific search key and location
-     
+
      - Parameter searchString: The search key used to get the search result.
      - Parameter coordinate: The coordinates of the current user location when the search is performed
-     
+
      - Returns: An array `Place` objects if data is saved for the given search key and location. `nil` otherwise
      */
     func placesForSearchResult(searchString: String, coordinate: CLLocationCoordinate2D ) -> [Place]? {
-        
+
         if let sourceSearch = self.userLocationSearch(searchString: searchString, coordinate:coordinate, createNewIfNone:false)
         {
             if let places = sourceSearch.places {
                 return places.allObjects as? [Place]
             }
         }
-        
+
         self.rollbackDatabase()
         return nil
     }
-    
+
     //MARK: Private methods
-    
+
     private func saveDatabase(){
         if self.managedObjectContext.hasChanges {
             do {
@@ -132,18 +131,18 @@ class CoreDataHelper : NSObject {
             }
         }
     }
-    
+
     private func rollbackDatabase(){
         if self.managedObjectContext.hasChanges {
             self.managedObjectContext.rollback()
         }
     }
-    
+
     private func userLocationSearch(searchString: String, coordinate : CLLocationCoordinate2D, createNewIfNone: Bool) -> UserLocationSearch?
     {
         let search = self.searchForSearchString(searchString: searchString)
         let userLocation = self.userLocationForCoordinate(coordinate: coordinate)
-        
+
         let fetchRequest : NSFetchRequest<UserLocationSearch> = UserLocationSearch.fetchRequest()
         let predicate = NSPredicate(format:"searchTerm == %@ AND location == %@", search!, userLocation!)
         fetchRequest.predicate = predicate
@@ -154,18 +153,18 @@ class CoreDataHelper : NSObject {
                 userLocationSearch = fetchResult[0]
             }
         } catch {
-            
+
         }
-        
+
         if userLocationSearch == nil && createNewIfNone {
             userLocationSearch = NSEntityDescription.insertNewObject(forEntityName: "UserLocationSearch", into:self.managedObjectContext) as? UserLocationSearch
             userLocationSearch?.searchTerm = search
             userLocationSearch?.location = userLocation
         }
-        
+
         return userLocationSearch
     }
-    
+
     private func searchForSearchString(searchString: String) -> Search?{
         let fetchRequest : NSFetchRequest<Search> = Search.fetchRequest()
         let predicate = NSPredicate(format:"searchString ==[c] %@", searchString)
@@ -173,14 +172,14 @@ class CoreDataHelper : NSObject {
         var search : Search?
         do {
             let fetchResult =  try self.managedObjectContext.fetch(fetchRequest)
-            
+
             if fetchResult.count > 0  {
                 search = fetchResult[0]
             }
         } catch {
-            
+
         }
-        
+
         if search == nil
         {
             search = NSEntityDescription.insertNewObject(forEntityName: "Search", into: self.managedObjectContext) as? Search
@@ -188,7 +187,7 @@ class CoreDataHelper : NSObject {
         }
         return search
     }
-    
+
     private func userLocationForCoordinate(coordinate: CLLocationCoordinate2D) -> UserLocation?{
         let fetchRequest : NSFetchRequest<UserLocation> = UserLocation.fetchRequest()
         let threshold = 0.001
@@ -201,9 +200,9 @@ class CoreDataHelper : NSObject {
                 userLocation = fetchResult[0]
             }
         } catch {
-            
+
         }
-        
+
         if userLocation == nil
         {
             userLocation = NSEntityDescription.insertNewObject(forEntityName: "UserLocation", into: self.managedObjectContext) as? UserLocation
@@ -212,6 +211,5 @@ class CoreDataHelper : NSObject {
         }
         return userLocation
     }
-    
-}
 
+}
--- FindPlacesNearMeSwift/FindPlacesNearMeSwift/ViewController.swift
+++ FindPlacesNearMeSwift/FindPlacesNearMeSwift/ViewController.swift
@@ -17,24 +17,24 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var locationPermissionWarning: UILabel!
-    
+
     let annotationViewIdentifier = "MyAnnotation"
     var locationManager : CLLocationManager?
     var currentAnnotations : [MKAnnotation]?
     var currentSearchString : String?
     var dbhelper : CoreDataHelper?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.locationManager = CLLocationManager()
         self.locationManager?.delegate = self
-        
+
         if CLLocationManager.locationServicesEnabled()
         {
             self.locationManager?.desiredAccuracy = kCLLocationAccuracyBest
             self.locationManager?.requestWhenInUseAuthorization()
-            
+
             if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways ||
                 CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse
             {
@@ -46,44 +46,44 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             self.searchBar.isUserInteractionEnabled = false
             self.locationPermissionWarning.isHidden = false
         }
-        
+
         self.mapView.register(MKPinAnnotationView.self, forAnnotationViewWithReuseIdentifier: self.annotationViewIdentifier)
-        
+
         // Do any additional setup after loading the view.
         let delegate : AppDelegate  = UIApplication.shared.delegate as! AppDelegate
         self.dbhelper = CoreDataHelper(context: delegate.persistentContainer.viewContext)
     }
-    
-    
+
+
     ///The refreshButton reperforms the search given the search string that is currently in the search bar
     ///This is in case the user have changed location and wants to update the search result for user's new location
     @IBAction func refreshButtonHit(_ sender: Any) {
         self.performSearch()
     }
-    
-    ///The resetButton resets the map view to the initial view after a searcj
+
+    ///The resetButton resets the map view to the initial view after a search
     ///That is the mapView is zoomed in such a way that all search result pins are visible in the map
     @IBAction func resetButtonHit(_ sender: Any) {
         if let annotations = self.currentAnnotations, annotations.count > 0 {
-            
+
             for annotation in self.mapView.selectedAnnotations{
                 self.mapView.deselectAnnotation(annotation, animated: false)
             }
-            
+
             self.mapView.showAnnotations(annotations, animated: true)
         }
     }
-    
+
     //MARK: - MKMapViewDelegate methods
-    
+
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
+
         if annotation is MKUserLocation {
             return nil
         }
-        
+
         var annotationView  = mapView.dequeueReusableAnnotationView(withIdentifier: self.annotationViewIdentifier)
-        
+
         if annotationView == nil{
             annotationView = MKPinAnnotationView(annotation:annotation, reuseIdentifier:self.annotationViewIdentifier)
         }
@@ -93,15 +93,16 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         annotationView?.canShowCallout = true
         return annotationView
     }
-    
+
     func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        //show end search state after the annotations are already shown in view
         self.didEndSearch()
-        //enable only the reset b
+        //enable only the reset button if there are annotations shown
         self.resetButton.isEnabled = true
     }
-    
+
     //MARK: CLLocationManagerDelegate methods
-    
+
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways ||
             CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse
@@ -112,7 +113,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             if self.currentSearchString != nil {
                 self.refreshButton.isEnabled = true
             }
-                        
+
             if let annotations = self.currentAnnotations {
                 self.mapView.addAnnotations(annotations)
                 self.mapView .showAnnotations(annotations, animated: true)
@@ -128,96 +129,119 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                 self.mapView.removeAnnotations(annotations)
                 self.resetButton.isEnabled = false
             }
-            
-    
         }
     }
-    
+
     //MARK: UISearchBarDelegate methods
-    
+
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
         let textToSearch = searchBar.text?.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
-        
+
         if textToSearch == nil || textToSearch?.count == 0
         {
             self.currentSearchString = nil;
             return
         }
-        
+
         self.currentSearchString = textToSearch
         self.performSearch()
-        
+
     }
-    
+
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
     }
-    
+
     //MARK: UtilityMethods
     /**
-     Adds the annotation view to the map view
-     */
-    func addAnnotationToMapView(annotations : [MKAnnotation]?){
-        if annotations == nil || annotations?.count == 0
-        {
-            self.showAlert(message:"Place with name is not found within the area" )
-            self.didEndSearch()
-            return
-        }
-        
+    Adds array of annotations to the map view
+    Sets the map to show all the annotations added
+
+		-Parameter annotations: array of annotations to add to the mapView
+    */
+    func addAnnotationToMapView(annotations : [MKAnnotation]){
         self.currentAnnotations = annotations
-        self.mapView.addAnnotations(annotations!)
-        self.mapView .showAnnotations(annotations!, animated: true)
+        self.mapView.addAnnotations(annotations)
+        self.mapView .showAnnotations(annotations, animated: true)
     }
-    
+
+    /**
+    Performs the search for the current search string and show the result
+    in the map
+    It first tries to find if there is already a cached search result for the
+    given search string in the current user location in the database.
+    If there is no cached result, triggers the search using the places demo API
+    */
     func performSearch() {
         if let searchString = self.currentSearchString {
             if let annotations = self.currentAnnotations {
                 self.mapView.removeAnnotations(annotations)
                 self.currentAnnotations = nil
             }
-            
+
             //check first if search result for the given key and location is already saved in database
             //use saved result instead of querying in places demo api if there is saved result
             if let places = self.dbhelper?.placesForSearchResult(searchString: searchString, coordinate:self.mapView.userLocation.coordinate) {
-                self.currentAnnotations = self.annotationsForPlaces(places: places)
-                self.addAnnotationToMapView(annotations:self.currentAnnotations)
+                if let annotations =  self.annotationsForPlaces(places: places){
+                    self.currentAnnotations = annotations
+                    self.addAnnotationToMapView(annotations:annotations)
+                }
             } else {
                 //else perform a new search to the places demo api
+                //disable the buttons
                 self.refreshButton.isEnabled = false
                 self.resetButton.isEnabled = false
+                //start searching indicator
                 self.searchingIndicator.startAnimating()
-                
+                //disable user interaction for mapView and searchBar
                 self.mapView.isUserInteractionEnabled = false
                 self.searchBar.isUserInteractionEnabled = false
+
                 self.performSearchForString(searchString: searchString)
             }
         }
     }
-    
+
+    /**
+    Performs the search using the places demo API  for the given searchString
+
+    - Parameter searchString: place identifier to be searched
+    around the current user location
+    */
     func performSearchForString(searchString : String)
     {
-        guard let url = self.placeRestAPIURLForSearchString(searchString: searchString)
+        //get user coordinate
+        let coordinate = self.mapView.userLocation.coordinate;
+
+        //form the REST API url for the places demo API
+        guard let url = self.placeRestAPIURLForSearchString(searchString: searchString, coordinate: coordinate)
             else {
+              //if search url is not properly formed, show alert that search has failed
+              //and end search
                 self.showAlert(message:"Search failed")
                 self.didEndSearch()
                 return
         }
-        
-        let coordinate = self.mapView.userLocation.coordinate;
-        
-        let dataTask = URLSession.shared.dataTask(with: url) { ( data, response, error) in
-            
+
+        //perform search via REST API using place demo API
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+
             if  let httpresponse = response as? HTTPURLResponse {
                 if httpresponse.statusCode == 200 {
                     do {
                         if let jsonData = data {
                             let result = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String:AnyObject]
-                            
+
                             if let annotations = self.annotationsForSearchResult(searchResult: result, searchString: searchString, coordinate: coordinate) {
-                                
+
                                 DispatchQueue.main.async {
+                                    if annotations.count == 0
+                                    {
+                                        self.showAlert(message:"Place with name is not found within the area" )
+                                        self.didEndSearch()
+                                        return
+                                    }
                                     self.addAnnotationToMapView(annotations: annotations)
                                 }
                                 return
@@ -232,58 +256,81 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                 self.didEndSearch()
             }
         }
-        
+
         dataTask.resume()
     }
-    
-    func placeRestAPIURLForSearchString(searchString : String) ->URL?
+
+    /**
+    Form the REST API URL using the places demo API  for the given searchString
+    - Parameter searchString: place identifier to be searched
+    around the current user location
+
+      - Parameter coordinate: coordinates for the current user location
+    */
+    func placeRestAPIURLForSearchString(searchString : String, coordinate: CLLocationCoordinate2D) ->URL?
     {
         guard let restAPIURLComponents  = NSURLComponents(string:"https://places.demo.api.here.com/places/v1/discover/search") else {
             return nil
         }
         //add search parameters
         let searchItem = URLQueryItem(name:"q", value: searchString)
-        
+
         //maximum of 10 results
         let searchSize = URLQueryItem(name:"size", value:"10")
-        
-        let lat = self.mapView.userLocation.coordinate.latitude
-        let lon = self.mapView.userLocation.coordinate.longitude
-        
+
+        let lat = coordinate.latitude
+        let lon = coordinate.longitude
+
         //search area within 5km around the current location
         let areaSpecification = "\(lat),\(lon);r=5000"
-        
+
         let searchArea = URLQueryItem(name:"in", value:areaSpecification)
-        
+
         //authentication credentials for demo account
         let app_id = URLQueryItem(name:"app_id", value:"DemoAppId01082013GAL")
         let app_code = URLQueryItem(name:"app_code", value:"AJKnXv84fjrb0KIHawS0Tg")
-        
+
         restAPIURLComponents.queryItems = [searchItem, searchSize, searchArea, app_id, app_code]
-        
+
         return restAPIURLComponents.url
     }
-    
+
+    /**
+    Creates an array of annotations given the search result from the places demo API
+    - Parameter searchResult: dictionary form of the JSON result of the query to
+    the places demo API
+
+    - Parameter searchString: place identifier searched
+    - Parameter coordinate: ser location searched
+
+    - Returns: Array of annotations if data of surrounding places is extracted from
+    the searchResult, nil otherwise
+    */
     func annotationsForSearchResult(searchResult : [String: AnyObject]?, searchString : String, coordinate: CLLocationCoordinate2D) -> [MKAnnotation]? {
-        
+        //parse search result into an array of Place objects
         if let places = self.dbhelper?.placesForSearchResult(searchResult: searchResult, searchString:searchString, coordinate: coordinate)
         {
-            guard let annotations = self.annotationsForPlaces(places: places) else {
-                return nil
-            }
-            return annotations
+          //create annotations from array of place objects
+            return self.annotationsForPlaces(places: places)
         }
         return nil
     }
-    
+
+    /**
+    Creates an array of annotations given an array of Place objects
+
+    - Parameter places: array of place objects
+
+    - Returns: Array of annotations
+    */
     func annotationsForPlaces(places : [Place]?) -> [MKAnnotation]?{
         guard let placeArray = places else {
             return nil
         }
-        
+
         var annotations : [MKAnnotation] = []
         for place in placeArray {
-            
+
             var unit = "m"
             var unitDivisor = 1.0
             if place.distance >= 1000.0
@@ -292,36 +339,46 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                 unitDivisor = 1000.0
             }
             let distanceString = NSString(format:"%g", place.distance/unitDivisor)
-            
+
             let annotation = MKPointAnnotation()
             annotation.coordinate.latitude = place.latitude
             annotation.coordinate.longitude = place.longitude
             annotation.title =  (place.title ?? "") + ", "  + (place.vicinity ?? "")
             annotation.subtitle = "\(distanceString) \(unit) away"
-            
+
             annotations.append(annotation)
         }
-        
+
         return annotations
     }
-    
+
+    /**
+    Shows an alert for the given alert message
+
+    - Parameter message: message to display in alert
+    */
     func showAlert(message: String){
         let appName : String? = Bundle.main.infoDictionary?["CFBundleDisplayName"]  as? String ?? ""
-        
+
         let alert  = UIAlertController(title:appName, message:message, preferredStyle: UIAlertController.Style.alert)
-        
+
         let action = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel) { (alertaction) in
-            
+
         }
-        
+
         alert.addAction(action)
-        
+
         self.present(alert, animated: true) {
             //Do nothing
         }
-        
     }
-    
+
+    /**
+    Sets the state of the UI when a search has ended.
+    * Stop searching searchingIndicator
+    * enable refreshButton
+    * enable user interaction for mapView and searchBar
+    */
     func didEndSearch()
     {
         self.searchingIndicator.stopAnimating()
@@ -330,4 +387,3 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         self.searchBar.isUserInteractionEnabled = true
     }
 }
-
